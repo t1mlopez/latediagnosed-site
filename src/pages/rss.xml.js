d6733ccce766645d543edc523b88d5a8d63f8b62
Tim Lopez <tim@timlopez.com>
2026-06-26 19:17:51 -0700

    Clean up old starter routes and update RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,26 @@ import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const articles = await getCollection('articles');
+
+	const publishedArticles = articles
+		.filter((article) => article.data.status === 'published')
+		.sort((a, b) => {
+			const aDate = a.data.publishDate ? new Date(a.data.publishDate).getTime() : 0;
+			const bDate = b.data.publishDate ? new Date(b.data.publishDate).getTime() : 0;
+
+			return bDate - aDate;
+		});
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
+		items: publishedArticles.map((article) => ({
+			title: article.data.title,
+			description: article.data.description,
+			pubDate: article.data.publishDate,
+			link: `/articles/${article.id}/`,
 		})),
 	});
 }
