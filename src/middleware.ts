5259e34f0f0c039f58eb758c42eaf7744f5eec90
Tim Lopez <tim@timlopez.com>
2026-08-29 16:48:36 -0700

    Route production domains to Cloudflare Worker

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,12 @@ import { clearSession, readSession } from './lib/auth/cookies';
 import { SESSION_COOKIE } from './lib/auth/config';
 
 export const onRequest = defineMiddleware(async (context, next) => {
+  if (context.url.hostname === 'www.latediagnosed.org') {
+    const canonicalUrl = new URL(context.url);
+    canonicalUrl.hostname = 'latediagnosed.org';
+    return context.redirect(canonicalUrl.toString(), 308);
+  }
+
   try {
     const session = await readSession(context.cookies);
     context.locals.user = session?.user ?? null;
